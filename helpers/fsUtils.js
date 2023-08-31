a7f137ebb00c67105aadae46f18e812d3b6422a7
eliabn <30064396@tafe.wa.edu.au>
2023-08-31 15:54:37 +0930

    Updated fsUtils.js file

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
@@ -7,9 +17,10 @@ const readAndAppend = (content, file) => {
     } else {
       const parsedData = JSON.parse(data);
       parsedData.push(content);
-      writeToFile(parsedData,file );
+      writeToFile(file, parsedData);
     }
   });
 };
+
 
 module.exports = { readAndAppend };
